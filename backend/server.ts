826e94f9257e1763dc61b0a40b075006d5f0db6f
LukeT2340 <lukethompson220@gmail.com>
2024-06-07 19:44:04 +1000

    added code to connect to database. use cors middleware

--- backend/server.ts
+++ backend/server.ts
@@ -1,16 +1,51 @@
-import express, { Request, Response } from 'express';
+// server.ts
+import express, { Request, Response } from 'express'
+const cors = require('cors')
+const mongoose = require('mongoose')
 require("dotenv").config()
+
+// Connection URI for your MongoDB database
+const mongoURI = process.env.mongooseURI || ""
+
+// Connect to MongoDB
+mongoose.connect(mongoURI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+// Get the default connection
+const db = mongoose.connection
+
+// Event handlers for database connection
+db.on('connected', () => {
+  console.log('Connected to MongoDB')
+})
+
+db.on('error', (err: string) => {
+  console.error('MongoDB connection error:', err)
+})
+
+db.on('disconnected', () => {
+  console.log('Disconnected from MongoDB')
+})
 
 // Create Express app
 const app = express();
+
+// Only allow requests from the following
+app.use(cors({
+    origin: process.env.FRONTEND_URL,
+    credentials: true
+}))
+
 const port = process.env.PORT || 3000;
 
 // Define a route handler for the root path
 app.get('/', (req: Request, res: Response) => {
-    res.send('Hello World!');
+    res.send('Hello World!')
 });
 
 // Start the Express server
 app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
+    console.log(`Server is running on http://localhost:${port}`)
 });
